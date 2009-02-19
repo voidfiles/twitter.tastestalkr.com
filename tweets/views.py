2333b51409157e7390069c43defec4d86954e49f
Alexander Kessinger <voidfiles@gmail.com>
2009-02-19 15:35:32 -0800

    adding filters

--- tweets/views.py
+++ tweets/views.py
@@ -2,10 +2,20 @@
 
 from django.shortcuts import render_to_response
 
-def filter(request,action = "last", value="24hours"):
+def filter(request,time = "hour", value="24"):
     from twmusic.tweets.models import Tweet
+    ACCEPTABLE_TIMES = [
+        "hour",
+        "day",
+        "month",
+        "second",
+        "week",
+        "year",
+    ]
+    assert time.lower() in ACCEPTABLE_TIMES, "%s is not a support paramater" % (time)
     
-    ranked_tweets = Tweet.objects.top_tweets(limit = 10, with_in_hours= 24)
+    value = int(value)
+    ranked_tweets = Tweet.objects.top_tweets(limit = 10, time = time, value= value)
     
     return render_to_response('base.html',
                               {"ranked_tweets":ranked_tweets} )
